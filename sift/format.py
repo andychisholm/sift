f400f7910c633dccfb74cd0bd093ae57f45d4989
Andrew Chisholm <andy.chisholm.89@gmail.com>
2015-12-23 11:50:15 +1100

    Enable single field storage for redis formatter to reduce overhead in simple models

--- sift/format.py
+++ sift/format.py
@@ -50,25 +50,36 @@ class JsonFormat(ModelFormat):
 
 class RedisFormat(ModelFormat):
     """ Format model output as redis protocol SET commands """
-    def __init__(self, prefix, serializer):
-        self.prefix = prefix
+    def __init__(self, prefix, serializer, field):
+        if serializer == 'raw' and not field:
+            raise Exception("Target field required for raw serializer")
 
+        self.prefix = prefix
+        self.field = field
         self.serializer = {
             'json': json.dumps,
             'msgpack': lambda o: base64.b64encode(msgpack.dumps(o)),
-            'pickle': lambda o: base64.b64encode(pickle.dumps(o, -1))
+            'pickle': lambda o: base64.b64encode(pickle.dumps(o, -1)),
+            'raw': lambda o: o
         }[serializer]
+
+    def to_value(self, item):
+        if self.field:
+            item = unicode(item[self.field])
+        else:
+            item.pop('_id', None)
+        return self.serializer(item)
 
     def __call__(self, model):
         cmd = '\r\n'.join(["*3", "$3", "SET", "${}", "{}", "${}", "{}"])+'\r'
-
         return model\
-            .map(lambda i: ((self.prefix+i['_id'].replace('"','\\"')).encode('utf-8'), self.serializer(i)))\
+            .map(lambda i: ((self.prefix+i['_id'].replace('"','\\"')).encode('utf-8'), self.to_value(i)))\
             .map(lambda (t, c): cmd.format(len(t), t, len(c), c))
 
     @classmethod
     def add_arguments(cls, p):
         p.add_argument('--prefix', required=False, default='', metavar='PREFIX')
-        p.add_argument('--serializer', choices=['json', 'pickle', 'msgpack'], required=False, default='json', metavar='SERIALIZER')
+        p.add_argument('--serializer', choices=['json', 'pickle', 'msgpack', 'raw'], required=False, default='json', metavar='SERIALIZER')
+        p.add_argument('--field', required=False, metavar='FIELD_TO_SERIALIZE')
         p.set_defaults(fmtcls=cls)
         return p
