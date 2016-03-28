5702d5905fa8d65c7b9a11b7c25d8bd9eb4865ca
Andrew Chisholm <andy.chisholm.89@gmail.com>
2016-03-28 19:18:18 +1100

    Improved sentence tokenisation

--- sift/util.py
+++ sift/util.py
@@ -7,11 +7,35 @@ def ngrams(text, max_n=1, min_n=1):
         for n in en.ngrams(text, n=i+1):
             yield ' '.join(n)
 
-SENT_RE = re.compile('((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|[\?!])\s)|(\s*\n\s*)')
+
+# sentences can't end with a single lowercase letter
+SENT_NO_END_LC = "(?<!(\s[a-z]\.))"
+
+# abbreviation sequences don't delimit
+ABBREV = "(?<!\w\.\w.)"
+
+SENT_POS_HEUR = [
+    "(?<=\.|[\?!])"     # should end with punctuation
+]
+SENT_NEG_ABBREVS = '(('+')|('.join([
+    "[Ii]nc",
+    "[Pp]ty",
+    "[Ll]td",
+]) + '))'
+SENT_NEG_HEUR = [
+    "(?<!(\s[a-z]\.))",  # can't end with a single lowercase letter (e.g. "c.")
+    "(?<!\w\.\w.)",      # abbreviation sequences don't delimit (e.g. "e.g.")
+    "(?<![A-Z][a-z]\.)", # can't end in two character capitalised word (e.g. "Ph.D")
+    "(?<!"+SENT_NEG_ABBREVS+"\.)" # can't end with a hardcoded abbreviation (e.g. "Inc.")
+]
+SENT_HEURISTICS = '('+''.join(SENT_NEG_HEUR+SENT_POS_HEUR)+')'
+
+SENT_RE = re.compile('('+SENT_HEURISTICS+'\s)|(\s*\n\s*)')
 def iter_sent_spans(text):
     last = 0
     for m in SENT_RE.finditer(text):
-        yield slice(last, m.start())
+        if last != m.start():
+            yield slice(last, m.start())
         last = m.end()
     if last != len(text):
         yield slice(last, len(text))
